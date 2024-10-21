468c08b62d0354f5efa935a76f005dfc197c48d9
Yannis <yannismosca@gmail.com>
2024-10-21 10:29:00 +0000

    Push

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int	main(void)
 {
-	#define PRINTSIZEA(Type) printf("Size of a " #Type " is %zu\n", sizeof(Type))
-	#define PRINTSIZEAN(Type) printf("Size of an " #Type " is %zu\n", sizeof(Type))
+	#define PRINTSIZEA(Type) printf("Size of a " #Type ": %zu byte(s)\n", sizeof(Type))
+	#define PRINTSIZEAN(Type) printf("Size of an " #Type ": %zu byte(s)\n", sizeof(Type))
 	PRINTSIZEA(char);
 	PRINTSIZEA(long int);
 	PRINTSIZEA(long long int);
