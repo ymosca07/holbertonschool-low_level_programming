bd88a05ceae5208a02da7d9456afe2bef31498ff
Yannis <yannismosca@gmail.com>
2024-10-23 07:31:03 +0000

    Push

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "1-main.c"
 
 void print_alphabet(void)
 {
--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,10 +1,11 @@
-#include "1-alphabet.c"
+#include "main.h"
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     print_alphabet();
