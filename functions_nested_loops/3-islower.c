3efcd66bdc1b5c4fd224ae5c311c64df0e587421
Yannis <yannismosca@gmail.com>
2024-10-23 10:55:35 +0000

    Push

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,15 @@
+
+/**
+ * _islower - Check desc.
+ * @c: input
+ * Description: Return 1 if lowercase
+ * Return: 0 (otherwise) and 1 (lowercase)
+ */
+
 int _islower(int c)
 {
-	
-	if(c == 'c')
+	if (c >= 'a' && c <= 'z')
 	return (1);
 
-	return (0);	
+	return (0);
 }
