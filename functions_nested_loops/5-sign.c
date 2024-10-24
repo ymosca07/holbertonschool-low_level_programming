8503076ad8c18707a58f0239486c19459c054445
Yannis <yannismosca@gmail.com>
2024-10-24 12:39:35 +0000

    Push

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		write(1, "+, ", 3);
+		write(1, "+", 1);
 		return (1);
 	}
 	if (n == 0)
 	{
-		write(1, "0, ", 3);
+		write(1, "0", 1);
 		return (0);
 	}
 	if (n < 0)
 	{
-		write(1, "-, ", 3);
+		write(1, "-", 1);
 		return (-1);
 	}
 	return (1);
