5ffe621dc7c6f7bceef6209c03f1afa9ad0d9b00
Yannis <yannismosca@gmail.com>
2024-10-24 13:03:58 +0000

    Push

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * print_last_digit - check the description
+ * @r: input
+ * Description: last digit of v
+ * Return: v.
+ */
+
 int print_last_digit(int r)
 {
 	int v;
 
 	v = r % 10;
 
-	printf("%u", v);
+	return (v);
 }
