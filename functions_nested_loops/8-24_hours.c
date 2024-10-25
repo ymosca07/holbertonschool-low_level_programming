fda8fecefa03a98f24cc38ec9db4e949719ff7ee
Yannis <yannismosca@gmail.com>
2024-10-25 10:58:59 +0000

    PUSHSHSHSSHSHS

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - check the description
- * @i, j : input
+ * @h, @m : input
  * Description: print 24 hours
  * Return : time of a day
  */
@@ -18,11 +18,11 @@ void jack_bauer(void)
 		m = 0;
 		while (m < 60)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 			m++;
 		}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,4 +7,3 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void _putchar(void);
