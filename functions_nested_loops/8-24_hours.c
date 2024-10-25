2cf96c9daef61a41fc7520c0fd447652ca59a3b6
Yannis <yannismosca@gmail.com>
2024-10-25 10:52:41 +0000

    Push

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+/**
+ * jack_bauer - check the description
+ * @i, j : input
+ * Description: print 24 hours
+ * Return : time of a day
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = 0;
+
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
