901791841d477f5b2aa076f031c6eb0673610377
Yannis <yannismosca@gmail.com>
2024-10-28 09:17:37 +0000

    Push

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * print_line - check the desc
+ * @n : input
+ * Desc: display underscore
+ * Return: display n times underscore
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= '0')
+	write(1, "\n", 1);
+
+	while (i <= n)
+	{
+		write(1, "_", 1);
+	}
+	write(1, "\n", 1);
+}
