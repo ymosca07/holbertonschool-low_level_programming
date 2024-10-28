387f2f9ff3588671b7e7f8cf13f1a98f08014e00
Yannis <yannismosca@gmail.com>
2024-10-28 09:28:13 +0000

    Push

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -12,14 +12,13 @@ void print_line(int n)
 	int i  = 0;
 
 	if (n <= '0')
-	write(1, "\n", 1);
-	else
-	{
-	while (i <= n)
+	write(1, "", 0);
+
+	while (i < n)
 	{
 		write(1, "_", 1);
 		i++;
 	}
 	write(1, "\n", 1);
-	}
+
 }
