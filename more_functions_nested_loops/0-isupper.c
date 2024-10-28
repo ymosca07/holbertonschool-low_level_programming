f767d2882df5b504bff7a71e6a947f1c6d6e1883
Yannis <yannismosca@gmail.com>
2024-10-28 08:36:31 +0000

    Push

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * _isupper - check the description.
+ * @c: input
+ * Description: chech uppercase or otherwise 
+ * Return: return 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	return (1);
+
+	return (0);
+}
