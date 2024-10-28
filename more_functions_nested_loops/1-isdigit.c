ac9e55872db7ee0a2f5bd3e4e5acca221ac72d29
Yannis <yannismosca@gmail.com>
2024-10-28 08:42:03 +0000

    lkh

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+/**
+ * _isdigit - check the desc
+ * @c: input
+ * Desc: check digit or otherwise
+ * Return: return 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	return (1);
+
+	return (0);
+}
