e1a18002fa90772740fd7747911176f495ce4702
Yannis <yannismosca@gmail.com>
2024-10-28 09:00:52 +0000

    oig

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * print_most_numbers - check the desc
+ * Desc: digit
+ * Return: digit nothing 4 and 2
+ */
+
+void print_most_numbers(void)
+{
+	write(1, "01356789\n", 9);
+}
