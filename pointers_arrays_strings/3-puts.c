4ca9c17fa33f97f9939c6bce055eb517b4427cc9
Yannis <yannismosca@gmail.com>
2024-10-29 10:43:01 +0000

    Push

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,20 @@
+/**
+ * _puts - check the desc
+ * @str: input
+ * Desc: return string
+ * Return: string
+ */
+
 #include <unistd.h>
 
 void _puts(char *str)
 {
-	int i = 0;
+	int i = 0 + 48;
 
 	while (str[i] != '\0')
 	{
 		write(1, &i, 1);
 		i++;
 	}
+	write(1, "\n", 1);
 }
