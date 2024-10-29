233a50e21f4536b79e1a5b024ce31017c67e6cc9
Yannis <yannismosca@gmail.com>
2024-10-29 09:12:55 +0000

    Push

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+/**
+ * print_triangle - check the desc
+ * @size: input
+ * Desc: return #
+ * Return: print #
+ */
+
+void print_triangle(int size)
+{
+	int i = 0; /* Condition d'arret */
+	int v; /* Positionner les espaces */
+
+	if (size <= 0)
+	write(1, "\n", 1);
+	else
+	{
+		while (i < size)
+		{
+			v = 0;
+			while (v < size - i)
+			{
+				write(1, " ", 1);
+				v++;
+			}
+			while (v <= size)
+			{
+				write(1, "#", 1);
+				v++;
+			}
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+}
