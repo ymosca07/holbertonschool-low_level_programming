a94722b54a8919527447b5f10fca982a1e9a7dc1
Yannis <yannismosca@gmail.com>
2024-10-29 11:21:04 +0000

    oderghfpeghdoegheoergweiederydevfdogeoeb

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -13,12 +13,11 @@ void print_rev(char *s)
 
 	while (s[i] != '\0')
         {
-               	i++;
-        }
-	while (i > 0)
+		i++;
+	}
+	while (i >= 0)
 	{
 		write(1, &s[i], 1);
 		i--;
 	}
-	write(1, "\n", 1);
 }
