a1b2762b20bb2f559265ebabe174b9b756a855a5
Yannis <yannismosca@gmail.com>
2024-10-29 09:46:25 +0000

    Push

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - check the desc
+ * @a: input
+ * @b: input
+ * Desc: swap value
+ * Return: values swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	c = *a;
+	*a = *b;
+	*b = c;
+}
