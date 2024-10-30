9966443040759af476e8a635a011555f5e65ba78
Yannis <yannismosca@gmail.com>
2024-10-30 10:50:53 +0000

    Push

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
 
+/**
+ * _strlen - check the desc
+ * @str: input
+ * Desc: print lenght
+ * Return: i
+ */
+
 int _strlen(char *str)
 {
 	int i = 0;
@@ -11,14 +18,33 @@ int _strlen(char *str)
 	return (i);
 }
 
+/**
+ * puts_half - check the desc
+ * @str: input
+ * Desc: print string's half
+ * Return: string
+ */
+
 void puts_half(char *str)
 {
-	int n = (_strlen(str) - 1) / 2;
+	int n = (_strlen(str)) - 1 / 2;
+	int p = (_strlen(str)) / 2;
 
-	while (str[n] != '\0')
+	if (_strlen(str) % 3 == 0)
 	{
-		n++;
-		write(1, &str[n], 1);
+		while (str[n] != '\0')
+		{
+			write(1, &str[n], 1);
+			n++;
+		}
+	}
+	else if (_strlen(str) % 2 == 0)
+	{
+		while (str[p] != '\0')
+		{
+			write(1, &str[p], 1);
+			p++;
+		}
 	}
 	write(1, "\n", 1);
 }
