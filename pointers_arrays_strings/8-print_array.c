3f70fe25b7f0267fd734331c2fbcc0bb52d642f2
Yannis <yannismosca@gmail.com>
2024-10-30 13:59:17 +0000

    Push

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_array - check the desc
+ * @a: input
+ * @n: input
+ * Desc: print int's value
+ * Return: value
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+		i++;
+	}
+
+}
