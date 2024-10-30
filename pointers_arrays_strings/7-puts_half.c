9e7cd0f12e866aa60f7bf118bd6b906aeafcaf3e
Yannis <yannismosca@gmail.com>
2024-10-30 10:25:24 +0000

    Push

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void puts_half(char *str)
+{
+	int n = (_strlen(str) - 1) / 2;
+
+	while (str[n] != '\0')
+	{
+		write(1, &str[n], 1);
+		n++;
+	}
+	write(1, "\n", 1);
+}
