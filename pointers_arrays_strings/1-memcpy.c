175438d92a93a695bcefd0473c7ad4a96c9e6a84
Yannis <yannismosca@gmail.com>
2024-11-06 09:03:27 +0000

    Push

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,20 @@
+/**
+ * _memcpy - check the desc
+ * @dest: destination
+ * @src: source
+ * @n: size
+ * Desc: return dest
+ * Return: dest
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	return (dest);
 }
