c57e1c167ed8e2d62e5d04aeff17e55687af8072
Yannis <yannismosca@gmail.com>
2024-11-06 10:17:19 +0000

    Push

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while ((s[i] != '\0') && (s[i] != c))
 	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
 		i++;
 	}
-	return (0);
+
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (0);
 }
