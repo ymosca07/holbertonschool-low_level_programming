08f82739725fdba79a1ddde2b68db369aa39373e
Yannis <yannismosca@gmail.com>
2024-11-07 14:24:10 +0000

    Push

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check the desc
+ * @s: string
+ * Desc: strlen
+ * Return: i
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s));
+}
