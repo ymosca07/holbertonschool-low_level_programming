fb980868d485b24e8acd0f1fc2df21006628fc2a
Yannis <yannismosca@gmail.com>
2024-11-07 14:07:52 +0000

    Push

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
 
+/**
+ * _print_rev_recursion - check the desc
+ * @s: string
+ * Desc: return string
+ * Return: sting
+ */
+
 void _print_rev_recursion(char *s)
 {
 	int i = 0;
