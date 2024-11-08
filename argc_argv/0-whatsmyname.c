aa5110baf0dc5281ca44d6f06a9f21da7f2c8d5e
Yannis <yannismosca@gmail.com>
2024-11-08 13:01:36 +0000

    Push

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: integer
+ * @argv: integer
+ * Desc: program name
+ * Return: name
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+}
