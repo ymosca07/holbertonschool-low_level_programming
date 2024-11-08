e0f698560a07663aff01cc6f64aeb34b6ad6095e
Yannis <yannismosca@gmail.com>
2024-11-08 14:39:35 +0000

    Push

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: integer
+ * @argv: integer
+ * Desc: program name
+ * Return: name
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
