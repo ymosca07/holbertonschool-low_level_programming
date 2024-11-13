049acec127612558725b3a7793916439efb53d0c
Yannis <yannismosca@gmail.com>
2024-11-13 14:24:33 +0000

    Push

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - check the desd
+ * @str: string
+ * Desc: cpy a string enter in parameter
+ * Return: tab
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * _strdup - check the desd
  * @str: string
  * Desc: cpy a string enter in parameter
@@ -12,12 +30,13 @@ char *_strdup(char *str)
 	int i = 0;
 	char *tab;
 
-	tab = malloc(sizeof(*tab) * (*str));
+	tab = malloc(sizeof(char) * _strlen(str) + 1);
 
 	while (str[i] != '\0')
 	{
 		tab[i] = str[i];
 		i++;
 	}
+	tab[i] = '\0';
 	return (tab);
 }
