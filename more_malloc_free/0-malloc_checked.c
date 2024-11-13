229efd8618e4e086b63d396e435f359698f33d79
Yannis <yannismosca@gmail.com>
2024-11-13 10:56:04 +0000

    Push

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 
+/**
+ * malloc_checked - check the code
+ * @b: unsigned integer
+ * Desc: check malloc
+ * Return: nothing
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	int *p;
 
 	p = malloc(sizeof(int) * b);
 
-	if (!p)
+	if (p == NULL)
 	exit(98);
 }
