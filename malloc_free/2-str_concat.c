4bf1c00a36802202b02ec978bd54882742ed01d9
Yannis <yannismosca@gmail.com>
2024-11-13 14:46:38 +0000

    Push

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -35,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL || s2 == NULL)
 	return (NULL);
 
-	tab = malloc(sizeof(char) * (_strlen(s1) + (_strlen(s1) + 1)));
+	tab = malloc(sizeof(char) * (_strlen(s1) + (_strlen(s2) + 1)));
 
 	if (tab == NULL)
 	return (NULL);
