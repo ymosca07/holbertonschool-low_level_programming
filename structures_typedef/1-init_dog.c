fb6f7676dc12cd9b09f5c03940f30a2948716379
Yannis <yannismosca@gmail.com>
2024-11-15 13:23:19 +0000

    Psuh

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include "main.c"
 
 /**
  * init_dog - Initializes a variable of type struct dog
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#ifndef DOG_H
 #define DOG_H 
 
 /**
@@ -12,3 +13,5 @@ struct dog
 	float age;
 	char *owner;
 };
+
+#endif
