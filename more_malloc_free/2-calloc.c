cac64f195a8ed3f68f80a128587bba0e7261c2d1
Yannis <yannismosca@gmail.com>
2024-11-15 08:37:11 +0000

    Push

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,24 +1,28 @@
 #include <stdlib.h>
 
 /**
- * _calloc - check the code
- * @nmemb: integer
- * @size: integer
- * Desc: calloc
- * Return: tab
+ * *_calloc - function to allocates memory
+ * @nmemb: unsigned int type
+ * @size: unsigned int type
+ * Return: return pointer to array
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *tab;
+	char *ptr;
+	unsigned int count;
 
 	if (nmemb == 0 || size == 0)
-	return (NULL);
-
-	tab = calloc(nmemb, size);
-
-	if (tab == NULL)
-	return (NULL);
-
-	return (tab);
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	count = 0;
+	while (count < nmemb * size)
+	{
+		ptr[count] = 0;
+		count++;
+	}
+	return (ptr);
 }
