7867ba9b60148bb020df07a7896442f1f23683e6
Yannis <yannismosca@gmail.com>
2024-11-20 08:12:53 +0000

    Push

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include "main.c"
 
+/**
+ * print_name - check the desc
+ * @name: name of the person
+ * @f: function
+ * Return: Nothing.
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
