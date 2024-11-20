2357f0fefad39ad54a60fa285c84a41f8d47c380
Yannis <yannismosca@gmail.com>
2024-11-20 09:12:50 +0000

    Push

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,6 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
+	if (array == NULL || action == NULL)
+	{
+		return ;
+	}
+
 	while (i < size)
 	{
 		action(array[i]);
