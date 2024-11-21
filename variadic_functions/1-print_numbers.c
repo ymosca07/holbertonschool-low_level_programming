5f6b9a77cb1aa05e6f1fe667577ed9381065f5be
Yannis <yannismosca@gmail.com>
2024-11-21 15:18:24 +0000

    Push

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - check the code
+ * @separator: const char
+ * @n: integer
+ * Desc: print numbers
+ * Return: Always 0.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+	int res;
+	va_list args;
+
+	va_start(args, n);
+
+	while (i < n)
+	{
+		res = 0;
+		res = va_arg(args, int);
+
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%d%s", res, separator);
+		}
+		else if (separator != NULL)
+		{
+			printf("%d\n", res);
+		}
+
+		i++;
+	}
+	va_end(args);
+}
