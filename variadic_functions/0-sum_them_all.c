f580c8ec6476b8a771c4c81beb88d8f948e11c60
Yannis <yannismosca@gmail.com>
2024-11-21 14:23:37 +0000

    Push

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - check the code
+ * @n: integer
+ * Desc: sum
+ * Return: somme
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i = 0;
+	int somme = 0;
+
+	if (n == 0)
+	return (0);
+
+	va_list args;
+
+	va_start(args, n);
+
+	while (i < n)
+	{
+		somme += va_arg(args, int);
+		i++;
+	}
+	va_end(args);
+
+	return (somme);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_VARIADICS
+#define FUNCTION_VARIADICS
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
