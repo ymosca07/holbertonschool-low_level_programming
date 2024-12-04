ca0d5153e5e79c0cbb1aead5687376b78e2bb3fe
Yannis <yannismosca@gmail.com>
2024-12-04 15:10:32 +0000

    Push

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stddef.h>
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h-> str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
