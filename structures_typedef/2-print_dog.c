c10da3c3742ecbb4af0c637b8bc17cd9152fd8f6
Yannis <yannismosca@gmail.com>
2024-12-05 14:29:54 +0000

    Push

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,8 +12,6 @@ void print_dog(struct dog *d)
 
 		if (d->age != '\0')
 			printf("Age: %f\n", d->age);
-		else
-			printf("Age: (nil)\n");
 
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
