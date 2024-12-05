fb1cef813a2ff5f1924d895e558780414ef75889
Yannis <yannismosca@gmail.com>
2024-12-05 14:40:37 +0000

    Push

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,6 +15,13 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
+};
+
+typedef struct dog
+{
+        char *name;
+        float age;
+        char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
