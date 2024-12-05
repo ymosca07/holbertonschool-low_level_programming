aa91e3c5a8816d456cba17c56fa3b2773ee3bc4e
Yannis <yannismosca@gmail.com>
2024-12-05 11:50:39 +0000

    Push

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * printdlistint - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
