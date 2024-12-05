83ca0a2b34de108452824ed97517ab904c5baf89
Yannis <yannismosca@gmail.com>
2024-12-05 13:44:53 +0000

    Push

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_dnodeint - check the code
+ * @head: struct
+ * @n: integer
+ * Desc: print new node
+ * Return: head
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *i;
