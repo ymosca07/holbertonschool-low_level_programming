4deb7e1fa8dc56e8d4c2f8d6776a0a0dfcdf8649
Yannis <yannismosca@gmail.com>
2024-12-05 10:30:19 +0000

    Push

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,12 +10,10 @@
 
 void free_list(list_t *head)
 {
-	if (head != NULL)
+	while (head != NULL)
 	{
-		while (head != NULL)
-		{
-			head = head->next;
-			free(head);
-		}
+		head = head->next;
+		free(head);
+	}
 	free(head);
 }
