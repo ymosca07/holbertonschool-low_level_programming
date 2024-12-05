bc3bf5e4978b9d285f90c6ec9dcac49ce2f36c03
Yannis <yannismosca@gmail.com>
2024-12-05 12:38:20 +0000

    Push

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "main.c"
+#include <stdlib.h>
+
+/**
+ * dlistint_len - check the code
+ * @h: struct
+ * Desc: count lenght
+ * Return: count
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
