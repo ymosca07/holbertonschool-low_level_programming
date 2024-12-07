dc13e694771482ab4b1b4cf824b44684c6af96fb
Yannis <yannismosca@gmail.com>
2024-12-07 15:15:29 +0000

    Push

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_dlistint - check the code
+ * @head: struct
+ * Desc: free
+ * Return: nothing
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	while (head != NULL)
---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * get_dnodeint_at_index - gets the nth node of the list
+  * @head: head of the list
+  * @index: which node to stop at
+  * Return: pointer to the nth node
+  */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	
+	while (i < index)
+	{
+		if (head->next == NULL)
+		{
+			return (NULL);
+		}
+
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
