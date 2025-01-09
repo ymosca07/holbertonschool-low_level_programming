09cbf48cd3b931ccb76e2400041bb8d9bcea96a2
Yannis <yannismosca@gmail.com>
2025-01-09 14:52:39 +0100

    Push

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,16 +13,15 @@ char *_strcat(char *dest, char *src)
     int i = 0;
     int j = 0;
 
-    while(dest[i] != '\0')
+    while (dest[i] != '\0')
     {
         i++;
     }
 
-    while(src[j] != '\0')
+    while (src[j] != '\0')
     {
         dest[i + j] = src[j];
         j++;
     }
-
     return (dest);
 }
